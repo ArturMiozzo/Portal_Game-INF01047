6409386fa094e06270d3b29fb251137f978b9cfc
Eric <ericmathias2012@gmail.com>
2023-08-20 19:53:23 -0300

    Melhora da movimentação da camera

--- src/main.cpp
+++ src/main.cpp
@@ -187,8 +187,10 @@ float g_AngleY = 0.0f;
 float g_AngleZ = 0.0f;
 
 // botoes w a s d
-float b_forward = 0;
-float b_right = 0;
+bool b_forward = false;
+bool b_right = false;
+bool b_back = false;
+bool b_left = false;
 float speed = 0.03;
 // "g_LeftMouseButtonPressed = true" se o usuário está com o botão esquerdo do mouse
 // pressionado no momento atual. Veja função MouseButtonCallback().
@@ -337,6 +339,8 @@ int main(int argc, char* argv[])
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
 
+    float r = g_CameraDistance;
+    glm::vec4 camera_position_c  = glm::vec4(0,0,r,1.0f); // Ponto "c", centro da câmera
     // Ficamos em um loop infinito, renderizando, até que o usuário feche a janela
     while (!glfwWindowShouldClose(window))
     {
@@ -362,14 +366,16 @@ int main(int argc, char* argv[])
         // variáveis g_CameraDistance, g_CameraPhi, e g_CameraTheta são
         // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
         // e ScrollCallback().
-        float r = g_CameraDistance;
+
         float y = r*sin(g_CameraPhi);
         float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
         float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
 
+
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::vec4 camera_position_c  = glm::vec4(0,0,r,1.0f); // Ponto "c", centro da câmera
+
+        PrintVector(camera_position_c);
         //glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         //glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
         glm::vec4 camera_view_vector = glm::vec4(-x,-y,-z,0.0f);
@@ -377,7 +383,7 @@ int main(int argc, char* argv[])
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
         // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector, b_forward, b_right, speed);
+        glm::mat4 view = Matrix_Camera_View(&camera_position_c, camera_view_vector, camera_up_vector, b_forward, b_back, b_right, b_left, speed);
 
         // Agora computamos a matriz de Projeção.
         glm::mat4 projection;
@@ -1267,22 +1273,40 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
         fflush(stdout);
     }
 
-    if (key == GLFW_KEY_W)
+    if (key == GLFW_KEY_W && action == GLFW_PRESS)
     {
-        b_forward += 1;
+        b_forward = true;
+    }
+    if (key == GLFW_KEY_W && action == GLFW_RELEASE)
+    {
+        b_forward = false;
     }
 
-    if (key == GLFW_KEY_S)
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
     {
-        b_forward -= 1;
+        b_back = true;
     }
-    if (key == GLFW_KEY_D)
+    if (key == GLFW_KEY_S && action == GLFW_RELEASE)
     {
-        b_right += 1;
+        b_back = false;
     }
-    if (key == GLFW_KEY_A)
+
+    if (key == GLFW_KEY_D && action == GLFW_PRESS)
     {
-        b_right -= 1;
+        b_right = true;
+    }
+    if (key == GLFW_KEY_D && action == GLFW_RELEASE)
+    {
+        b_right = false;
+    }
+
+    if (key == GLFW_KEY_A && action == GLFW_PRESS)
+    {
+        b_left = true;
+    }
+    if (key == GLFW_KEY_A && action == GLFW_RELEASE)
+    {
+        b_left = false;
     }
 }
 
